de2fc93fd93013330fb0d615155968e1eaaa0988
Opjasson <azmitheusos333@gmail.com>
2024-11-11 15:04:20 +0700

    add midllewares

--- routes/web.php
+++ routes/web.php
@@ -5,8 +5,20 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Dashboard\HomeController;
 use Illuminate\Support\Facades\Auth;
 
+Route::get('/', function () {
+    return view("welcome");
+});
 
+// middleware
+Route::middleware(['guest'])->group(
+    function () {
+        Route::get('/login', [LoginController::class, 'index'])->name('auth.login');
+        Route::post('/login', [LoginController::class, 'action'])->name('auth.login.action');
+    }
+);
 
-Route::get('/login', [LoginController::class, 'index'])->name('auth.login');
-Route::post('/login', [LoginController::class, 'action'])->name('auth.login.action');
-Route::get('/dashboard',[HomeController::class, 'index'])->name('dashboard.home');
+Route::middleware(['auth'])->group(
+    function () {
+        Route::get('/dashboard', [HomeController::class, 'index'])->name('dashboard.home');
+    }
+);
