844a9a4578624092799482118464f9f0f9485321
Opjasson <azmitheusos333@gmail.com>
2024-11-29 15:06:23 +0700

    add controller type_category

--- app/Http/Controllers/Dashboard/Konten/MenuController.php
+++ app/Http/Controllers/Dashboard/Konten/MenuController.php
@@ -3,12 +3,16 @@
 namespace App\Http\Controllers\Dashboard\Konten;
 
 use App\Http\Controllers\Controller;
+use App\Models\Sambutan;
+use App\Models\Type_Category;
 use Illuminate\Http\Request;
 
 class MenuController extends Controller
 {
     //
     public function index(){
-        return view('dashboard.konten.menu');
+        return view('dashboard.konten.menu.menu',[
+            'types' => Type_Category::all()
+        ]);
     }
 }
--- resources/views/dashboard/konten/menu/menu.blade.php
+++ resources/views/dashboard/konten/menu/menu.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.dashboards.dashboard-layout')
 
 @section('title', 'Menu')
-    
+
 @section('content')
     <div class="md:w-full w-max h-screen">
         <nav class="flex justify-between bg-blue-400 px-5 py-2 md:w-full w-[28rem] md:text-base text-sm">
@@ -57,14 +57,11 @@
                         class="text-white mt-2 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-3 py-1.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Tambah</a>
                 </header>
 
-
-
-
                 <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
                     <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
                         <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                             <tr class="list-none">
-                                <th scope="col" class="px-6 py-3">
+                                <th scope="col" class="px-11 w-36 py-3">
                                     <li class="flex">
                                         No
                                         <a class="inline-block" href="#"><svg class="w-3 h-3 ms-1.5"
@@ -75,16 +72,16 @@
                                             </svg></a>
                                     </li>
                                 </th>
-                                <th scope="col" class="p-4">
+                                <th scope="col" class="py-3 w-3">
                                     <div class="flex items-center">
                                         <input id="checkbox-all-search" type="checkbox"
-                                            class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
+                                            class=" text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
                                         <label for="checkbox-all-search" class="sr-only">checkbox</label>
                                     </div>
                                 </th>
                                 <th scope="col" class="px-6 py-3">
                                     <li class="flex">
-                                        Product name
+                                        Judul
                                         <a class="inline-block" href="#"><svg class="w-3 h-3 ms-1.5"
                                                 aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor"
                                                 viewBox="0 0 24 24">
@@ -95,7 +92,7 @@
                                 </th>
                                 <th scope="col" class="px-6 py-3">
                                     <li class="flex">
-                                        Color
+                                        Tipe
                                         <a href="#"><svg class="w-3 h-3 ms-1.5" aria-hidden="true"
                                                 xmlns="http://www.w3.org/2000/svg" fill="currentColor"
                                                 viewBox="0 0 24 24">
@@ -106,7 +103,7 @@
                                 </th>
                                 <th scope="col" class="px-6 py-3">
                                     <li class="flex">
-                                        Category
+                                        Urutan
                                         <a href="#"><svg class="w-3 h-3 ms-1.5" aria-hidden="true"
                                                 xmlns="http://www.w3.org/2000/svg" fill="currentColor"
                                                 viewBox="0 0 24 24">
@@ -115,115 +112,50 @@
                                             </svg></a>
                                     </li>
                                 </th>
-                                <th scope="col" class="px-6 py-3">
-                                    <li class="flex">
-                                        Accessories
-                                        <a href="#"><svg class="w-3 h-3 ms-1.5" aria-hidden="true"
-                                                xmlns="http://www.w3.org/2000/svg" fill="currentColor"
-                                                viewBox="0 0 24 24">
-                                                <path
-                                                    d="M8.574 11.024h6.852a2.075 2.075 0 0 0 1.847-1.086 1.9 1.9 0 0 0-.11-1.986L13.736 2.9a2.122 2.122 0 0 0-3.472 0L6.837 7.952a1.9 1.9 0 0 0-.11 1.986 2.074 2.074 0 0 0 1.847 1.086Zm6.852 1.952H8.574a2.072 2.072 0 0 0-1.847 1.087 1.9 1.9 0 0 0 .11 1.985l3.426 5.05a2.123 2.123 0 0 0 3.472 0l3.427-5.05a1.9 1.9 0 0 0 .11-1.985 2.074 2.074 0 0 0-1.846-1.087Z" />
-                                            </svg></a>
-                                    </li>
-                                </th>
-
+                               
                             </tr>
                         </thead>
                         <tbody>
-                            <tr
-                                class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-                                <th scope="row"
-                                    class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                                    1
-                                </th>
-                                <td class="w-4 p-4">
-                                    <div class="flex items-center">
-                                        <input id="checkbox-table-search-1" type="checkbox"
-                                            class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
-                                        <label for="checkbox-table-search-1" class="sr-only">checkbox</label>
-                                    </div>
-                                </td>
-                                <th scope="row"
-                                    class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                                    Apple MacBook Pro 17"
-                                    <div class="text-xs font-normal flex gap-2">
-                                        <a class="hover:font-medium hover:underline" href="/">Ubah</a>
-                                        <a class="hover:font-medium hover:underline" href="/">Lihat</a>
-                                        <a class="hover:font-medium hover:underline" href="/">Hapus</a>
-                                    </div>
-                                </th>
-                                <td class="px-6 py-4">
-                                    Silver
-                                </td>
-                                <td class="px-6 py-4">
-                                    Laptop
-                                </td>
-                                <td class="px-6 py-4">
-                                    Yes
-                                </td>
+                            @foreach ($types as $type)
+                                <tr
+                                    class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+                                    <th scope="row"
+                                        class="px-12 w-36 py-3 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                        1
+                                    </th>
+                                    <td class="py-3 w-3">
+                                        <div class="flex items-center w-fit">
+                                            <input id="checkbox-table-search-1" type="checkbox"
+                                                class="w-4 h-4  text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
+                                            <label for="checkbox-table-search-1" class="sr-only">checkbox</label>
+                                        </div>
+                                    </td>
+                                    <th scope="row"
+                                        class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                        <p class="capitalize">{{ $type['type'] }}</p>
+                                        <div class="text-xs font-normal flex gap-2">
+                                            <a class="hover:font-medium hover:underline" href="/">Ubah</a>
+                                            <a class="hover:font-medium hover:underline" href="/">Lihat</a>
+                                            <a class="hover:font-medium hover:underline" href="/">Hapus</a>
+                                        </div>
+                                    </th>
+                                    <td class="px-6 py-4">
+                                        {{ $type['type'] }}
+                                    </td>
+                                    <td class="px-6 py-4">
+                                        Laptop
+                                    </td>
+                                    
 
 
-                            </tr>
-                            <tr
-                                class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-                                <th scope="row"
-                                    class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                                    2
-                                </th>
-                                <td class="w-4 p-4">
-                                    <div class="flex items-center">
-                                        <input id="checkbox-table-search-2" type="checkbox"
-                                            class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
-                                        <label for="checkbox-table-search-2" class="sr-only">checkbox</label>
-                                    </div>
-                                </td>
-                                <th scope="row"
-                                    class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                                    Microsoft Surface Pro
-                                </th>
-                                <td class="px-6 py-4">
-                                    White
-                                </td>
-                                <td class="px-6 py-4">
-                                    Laptop PC
-                                </td>
-                                <td class="px-6 py-4">
-                                    No
-                                </td>
+                                </tr>
+                            @endforeach
 
-
-                            </tr>
-                            <tr
-                                class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-                                <th scope="row"
-                                    class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                                    3
-                                </th>
-                                <td class="w-4 p-4">
-                                    <div class="flex items-center">
-                                        <input id="checkbox-table-search-3" type="checkbox"
-                                            class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
-                                        <label for="checkbox-table-search-3" class="sr-only">checkbox</label>
-                                    </div>
-                                </td>
-                                <th scope="row"
-                                    class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                                    Magic Mouse 2
-                                </th>
-                                <td class="px-6 py-4">
-                                    Black
-                                </td>
-                                <td class="px-6 py-4">
-                                    Accessories
-                                </td>
-                                <td class="px-6 py-4">
-                                    Yes
-                                </td>
-                            </tr>
                         </tbody>
                     </table>
                 </div>
-                <button type="button" class="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">Hapus</button>
+                <button type="button"
+                    class="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">Hapus</button>
 
             </main>
         </div>
