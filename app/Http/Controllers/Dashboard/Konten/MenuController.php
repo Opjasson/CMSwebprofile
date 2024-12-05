edaa0e2620bce94912071dcd369e721836dd2c49
Opjasson <azmitheusos333@gmail.com>
2024-12-05 13:41:11 +0700

    add slugify

--- app/Http/Controllers/Dashboard/Konten/MenuController.php
+++ app/Http/Controllers/Dashboard/Konten/MenuController.php
@@ -2,14 +2,16 @@
 
 namespace App\Http\Controllers\Dashboard\Konten;
 
-use App\Http\Controllers\Controller;
+use App\Models\Halaman;
+use App\Models\Sambutan;
+use App\Models\halaman_tipe;
+use Illuminate\Http\Request;
+use App\Models\Type_Category;
+use App\Models\HalamanKategory;
 use App\Models\halaman_category;
 use App\Models\halaman_kategori;
-use App\Models\halaman_tipe;
-use App\Models\HalamanKategory;
-use App\Models\Sambutan;
-use App\Models\Type_Category;
-use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
 
 class MenuController extends Controller
@@ -18,9 +20,7 @@ class MenuController extends Controller
     public function index(){
         return view('dashboard.konten.menu.menu',[
             'types' => Type_Category::all(),
-            'route' => route('dashboard.menu.tambah',[
-                'userId' => Auth::user()->id
-            ])
+            'route' => route('dashboard.menu.tambah')
         ]);
     }
 
@@ -36,6 +36,18 @@ class MenuController extends Controller
 
     public function fileDownload()
     {
-        return view('dashboard.konten.menu.form-addfileDownload');
+        return view('dashboard.konten.menu.form-addfileDownload',[
+            'action' => route('dashboard.menu.tambah.action')
+        ]);
+    }
+
+    public function createHal(Request $request)
+    {
+        DB::table('halaman')->insert([
+            'judul' => $request->judul,
+            // 'link' => $,
+            'sub_halaman' => $request->sub_halaman,
+            'urutan' => $request->urutan
+        ]);
     }
 }
--- resources/views/dashboard/konten/menu/form-addfileDownload.blade.php
+++ resources/views/dashboard/konten/menu/form-addfileDownload.blade.php
@@ -54,19 +54,57 @@
                 <h1 class="text-4xl font-semiboldbold mb-3">Menu Halaman</h1>
                 <h2 class="mb-5">Step 2 : Isi detail menu</h2>
                 <div>
-                    <form class="h-screen">
+                    <form class="h-screen" action="{{ $action }}" method="POST">
+                        @csrf
+                        @method('POST')
                         <div class="mb-5 flex gap-5 border-b-2 border-slate-500 pb-5">
-                          <label for="judul" class="block w-48 text-md font-medium text-gray-900 dark:text-white">Judul Halaman :</label>
-                          <input type="text" id="judul" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
+                            <label for="judul" class="block w-48 text-md font-medium text-gray-900 dark:text-white">Judul
+                                Halaman :</label>
+                            <input type="text" name="judul" id="judul"
+                                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                required />
                         </div>
-                        
-                        <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
-                        <a href="{{ route('dashboard.menu.tambah') }}" class="text-white bg-slate-700 hover:bg-slate-800 focus:ring-4 focus:outline-none focus:ring-slate-300 font-medium rounded-lg text-sm w-full sm:w-auto px-8 py-3 text-center dark:bg-slate-600 dark:hover:bg-slate-700 dark:focus:ring-slate-800">Back</a>
-                      </form>
+
+                        <input type="text" value="{{  }}" name="link">
+
+                        <div class="mb-5 flex gap-5 border-b-2 border-slate-500 pb-5">
+                            <label for="subMenu"
+                                class="block w-48 mb-2 text-md font-medium text-gray-900 dark:text-white">Sub-Menu Dari :</label>
+                            <select id="subMenu" name="sub_halaman"
+                                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                                <option value="0" selected>-</option>
+                                <option value="1">Beranda</option>
+                                <option value="2">Reels</option>
+                                <option value="3">Profile</option>
+                                <option value="4">Berita</option>
+                                <option value="5">Data</option>
+                                <option value="6">Staf</option>
+                                <option value="7">Galeri Perusahaan</option>
+                                <option value="8">Lowongan Pekerjaan</option>
+                            </select>
+                        </div>
+
+                        <div class="mb-5 flex gap-5 border-b-2 border-slate-500 pb-5">
+                            <label for="urutanHalaman"
+                                class="block w-48 mb-2 text-md font-medium text-gray-900 dark:text-white">Urutan Halaman :</label>
+                            <select id="urutanHalaman" name="urutan"
+                                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                                <option value="1" selected>1</option>
+                                <option value="2">1</option>
+                                <option value="3">3</option>
+                                <option value="4">4</option>
+                                <option value="5">5</option>
+                            </select>
+                        </div>
+
+                        <button type="submit"
+                            class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
+                        <a href="{{ route('dashboard.menu.tambah') }}"
+                            class="text-white bg-slate-700 hover:bg-slate-800 focus:ring-4 focus:outline-none focus:ring-slate-300 font-medium rounded-lg text-sm w-full sm:w-auto px-8 py-3 text-center dark:bg-slate-600 dark:hover:bg-slate-700 dark:focus:ring-slate-800">Back</a>
+                    </form>
                 </div>
             </main>
         </div>
 
     </div>
 @endsection
-
